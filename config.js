ab5c6fe90d1522d46767e052a5caefb9508ef1c0
nativeelaneeru <ir117737@gmail.com>
2026-09-09 17:07:15 +0530

    Add isolated B2B app configuration

---
+++ config.js
@@ -0,0 +1,9 @@
+window.NEL_B2B_CONFIG = {
+  apiUrl: 'https://script.google.com/macros/s/AKfycby4tO2Y1xWbVv1XqzbIYjuTKOt4XRWPzB9MseMC1x-qc8gbNYPvqvG1z6PuCPGZi5O2/exec',
+  appVersion: '1.0.0-b2b',
+  supportPhone: '7411807675',
+  supportWhatsApp: '917411807675',
+  company: 'Sri Govindadri Ventures',
+  channel: 'B2B',
+  environment: 'QA_PREVIEW'
+};
